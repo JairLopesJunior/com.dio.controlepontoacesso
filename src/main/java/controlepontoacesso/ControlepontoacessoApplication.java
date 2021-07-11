29ba915ebcf44a3f4910b65ac22195aaec9a7513
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-07-11 12:20:57 -0300

    Criado projeto no start.spring.io

---
+++ src/main/java/controlepontoacesso/ControlepontoacessoApplication.java
@@ -0,0 +1,13 @@
+package controlepontoacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlepontoacessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlepontoacessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/controlepontoacesso/ControlepontoacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package controlepontoacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlepontoacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
